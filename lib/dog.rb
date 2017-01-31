734379c2a9b308214ba9f97dbeb83afca5b49068
7s Dream <7seconddream@gmail.com>
2017-01-31 14:07:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  @@all = []
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+end
