577a34155e20e844bc329506122b43ab55f9e3a5
Tanuj Sharma <tanuj.goodboy77@gmail.com>
2017-12-21 11:24:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  @@all = []
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def name
+    @name
+  end
+  def self.all
+    @@all.each {|dog| puts dog.name}
+  end
+  def self.clear_all
+    @@all = []
+  end
+end
